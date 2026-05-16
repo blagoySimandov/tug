c4fb35920628d87fc78371d51ddb934eb9c1afc3
Blagoy Simandoff <baggy.simandoff@gmail.com>
2026-05-16 19:16:17 +0100

    chore: remove mock.py

--- tug-be/main.py
+++ tug-be/main.py
@@ -1,6 +1,5 @@
-
 from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 
 from models import ImportantMoment
@@ -30,22 +29,6 @@ app.include_router(events_router)
 @app.get("/")
 def read_root():
     return {"message": "tug backend is running"}
-
-
-@app.get("/videos")
-def get_videos():
-    return [
-        {
-            "id": 1,
-            "label": "Primary",
-            "url": "https://firebasestorage.googleapis.com/v0/b/tug-splitball.firebasestorage.app/o/arg_fr.mp4?alt=media&token=5ee16507-af66-4409-b9f1-2de014937342",
-        },
-        {
-            "id": 2,
-            "label": "Secondary",
-            "url": "https://firebasestorage.googleapis.com/v0/b/tug-splitball.firebasestorage.app/o/cr_bra.mp4?alt=media&token=c648c419-949b-4048-b880-1613227fdaf8",
-        },
-    ]
 
 
 @app.get("/{event_id}/important-moments", response_model=list[ImportantMoment])
